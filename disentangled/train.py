cbcdd905e59f3ef07d4ad8681b831430f84a816a
Jerome <jerome.ku@gmail.com>
2019-10-17 13:53:06 -0700

    update notebook

--- disentangled/train.py
+++ disentangled/train.py
@@ -11,7 +11,7 @@ from '.summary' import *
 tfd = tfp.distributions
 
 
-def train_step(*, model, optimizer, train_ds, test_ds, FLAGS, writer):
+def train_step(*, model, optimizer, train_ds, test_ds, FLAGS, writer, checkpoint_manager):
     step = tf.Variable(0, dtype=tf.int64, name="step")
 
     for inputs in train_ds.prefetch(buffer_size=None):
@@ -35,20 +35,20 @@ def train_step(*, model, optimizer, train_ds, test_ds, FLAGS, writer):
                 axis=0,
             )
             visualize_reconstruction(
-                inputs, reconstruction, name="train_reconstruction"
+                inputs, reconstruction, step, name="train_reconstruction"
             )
 
             static_prior = model.static_prior()
             _, dynamic_prior = model.sample_dynamic_prior(
-                FLAGS.num_samples, FLAGS.batch_size, sprites_data.length
+                FLAGS.num_samples, FLAGS.batch_size, sprites.length
             )
 
             if FLAGS.enable_debug_logging:
-                summarize_dist_params(static_prior, "static_prior")
-                summarize_dist_params(static_posterior, "static_posterior")
-                summarize_dist_params(dynamic_prior, "dynamic_prior")
-                summarize_dist_params(dynamic_posterior, "dynamic_posterior")
-                summarize_dist_params(likelihood, "likelihood")
+                summarize_dist_params(static_prior, "static_prior", step)
+                summarize_dist_params(static_posterior, "static_posterior", step)
+                summarize_dist_params(dynamic_prior, "dynamic_prior", step)
+                summarize_dist_params(dynamic_posterior, "dynamic_posterior", step)
+                summarize_dist_params(likelihood, "likelihood", step)
 
             static_prior_log_prob = static_prior.log_prob(static_sample)
             static_posterior_log_prob = static_posterior.log_prob(static_sample)
@@ -65,29 +65,29 @@ def train_step(*, model, optimizer, train_ds, test_ds, FLAGS, writer):
             if FLAGS.enable_debug_logging:
                 with tf.compat.v1.name_scope("log_probs"):
                     summarize_mean_in_nats_and_bits(
-                        static_prior_log_prob, FLAGS.latent_size_static, "static_prior"
+                        static_prior_log_prob, FLAGS.latent_size_static, "static_prior", step
                     )
                     summarize_mean_in_nats_and_bits(
                         static_posterior_log_prob,
                         FLAGS.latent_size_static,
-                        "static_posterior",
+                        "static_posterior", step
                     )
                     summarize_mean_in_nats_and_bits(
                         dynamic_prior_log_prob,
                         FLAGS.latent_size_dynamic * sprites.length,
-                        "dynamic_prior",
+                        "dynamic_prior", step
                     )
                     summarize_mean_in_nats_and_bits(
                         dynamic_posterior_log_prob,
                         FLAGS.latent_size_dynamic * sprites.length,
-                        "dynamic_posterior",
+                        "dynamic_posterior", step
                     )
                     summarize_mean_in_nats_and_bits(
                         likelihood_log_prob,
                         sprites.frame_size ** 2
                         * sprites.channels
                         * sprites.length,
-                        "likelihood",
+                        "likelihood", step
                     )
 
             elbo = tf.reduce_mean(
@@ -116,18 +116,18 @@ def train_step(*, model, optimizer, train_ds, test_ds, FLAGS, writer):
                     )
                 with tf.compat.v1.name_scope("vars"):
                     tf.compat.v2.summary.histogram(var.name, data=var, step=step)
-        optimizer.apply_gradients(grads_and_vars, step)
+        optimizer.apply_gradients(grads_and_vars)
 
-    is_log_step = step.numpy() % FLAGS.log_steps == 0
-    is_final_step = step.numpy() == FLAGS.max_steps
-    if is_log_step or is_final_step:
-        checkpoint_manager.save()
-        print("ELBO ({}/{}): {}".format(step.numpy(), FLAGS.max_steps, elbo.numpy()))
-        with tf.compat.v2.summary.record_if(True):
-            val_data = test_ds.take(20)
-            inputs = next(iter(val_data.shuffle(20).batch(3)))
-            visualize_qualitative_analysis(
-                inputs, model, FLAGS.num_reconstruction_samples
-            )
-
-    writer.flush()
+        is_log_step = step.numpy() % FLAGS.log_steps == 0
+        is_final_step = step.numpy() == FLAGS.max_steps
+        if is_log_step or is_final_step:
+            checkpoint_manager.save()
+            print("ELBO ({}/{}): {}".format(step.numpy(), FLAGS.max_steps, elbo.numpy()))
+            with tf.compat.v2.summary.record_if(True):
+                val_data = test_ds.take(20)
+                inputs = next(iter(val_data.shuffle(20).batch(3)))
+                visualize_qualitative_analysis(
+                    inputs, model, step, FLAGS.num_reconstruction_samples
+                )
+        step.assign_add(1)
+        writer.flush()
